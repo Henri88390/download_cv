0107792ac49936bc02f2acbdb328846e0a823222
Henri88390 <henri.lambert9@gmail.com>
2022-10-23 08:53:23 +0200

    add text for Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,14 +5,29 @@ interface FooterProps {}
 
 const Footer: FC<FooterProps> = () => (
   <div className="footer__wrapper">
-    Made by &nbsp;
-    <a
-      href="https://www.linkedin.com/in/henri-lambert-15a525252/"
-      rel="Linkedin"
-      className="__name-link"
-    >
-      Henri Lambert
-    </a>
+    <div className="__footer-line">
+      {" "}
+      Made by &nbsp;{" "}
+      <a
+        href="https://www.linkedin.com/in/henri-lambert-15a525252/"
+        rel="Linkedin"
+        className="__name-link"
+        target="__blank"
+      >
+        Henri Lambert
+      </a>
+    </div>
+    <div className="__footer-line">
+      This website was created using &nbsp;
+      <a
+        href="https://reactjs.org/"
+        rel="ReactJS"
+        className="__name-link"
+        target="__blank"
+      >
+        React
+      </a>
+    </div>
   </div>
 );
 
